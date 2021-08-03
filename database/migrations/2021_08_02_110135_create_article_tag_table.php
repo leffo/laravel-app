4dcdf1e9a66ab4b7249e86223a58c1e6168ad4a7
Alexandr Yakovlev <leffalt@gmail.com>
2021-08-03 15:08:08 +0700

    add cascade delete to migration

--- database/migrations/2021_08_02_110135_create_article_tag_table.php
+++ database/migrations/2021_08_02_110135_create_article_tag_table.php
@@ -16,10 +16,10 @@ class CreateArticleTagTable extends Migration
         Schema::create('article_tag', function (Blueprint $table) {
 //            $table->unsignedBigInteger('article_id');
 //            $table->foreign('article_id')->references('id')->on('articles');
-            $table->foreignId('article_id')->constrained();
+            $table->foreignId('article_id')->constrained()->onDelete('cascade');
 //            $table->unsignedBigInteger('tag_id');
 //            $table->foreign('tag_id')->references('id')->on('tags');
-            $table->foreignId('tag_id')->constrained();
+            $table->foreignId('tag_id')->constrained()->onDelete('cascade');
         });
     }
 
