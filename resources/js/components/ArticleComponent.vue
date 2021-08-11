61b9e78f8d290199ba6c1d1aa27f204be6a76d94
Alexandr Yakovlev <leffalt@gmail.com>
2021-08-11 11:52:47 +0700

    connect vue to project

---
+++ resources/js/components/ArticleComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+        <h1>Test!</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ArticleComponent"
+}
+</script>
+
+<style scoped>
+
+</style>
--- resources/views/app/article/show.blade.php
+++ resources/views/app/article/show.blade.php
@@ -1,7 +1,9 @@
 @extends('layouts.app')
 @section('content')
+<div id="app">
     <div class="row mt-5">
         <div class="col-12 p-3">
+            <article-component></article-component>
             <img src="{{ $article->img }}" class="border rounded mx-auto d-block" alt="...">
             <h5 class="mt-5">{{ $article->title }}</h5>
             <p>
@@ -15,6 +17,10 @@
             </p>
             <p class="card-text">{{ $article->body }}</p>
             <p>Опубликовано: <i>{{ $article->createdAtForHumans() }}</i></p>
+            <div class="mt-3">
+                <span class="badge bg-primary">{{ $article->state->likes }} <i class="far fa-thumbs-up"></i></span>
+                <span class="badge bg-danger">{{ $article->state->views }} <i class="far fa-eye"></i></span>
+            </div>
         </div>
     </div>
     <hr>
@@ -47,5 +53,9 @@
         </div>
 
     </div>
-    </template>
+</div>
+@endsection
+
+@section('vue')
+    <script src="{{ mix('/js/app.js') }}"></script>
 @endsection
